f92b7b0708b835fd934c2c164e03f65d12dc1e7d
Aylin <aylingrgn2@gmail.com>
2018-11-22 02:14:05 +0300

    A valid auth with firebae

--- entrecor/lib/main.dart
+++ entrecor/lib/main.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
-import 'homepage.dart';
-import 'signuppage.dart';
-import 'loginpage.dart';
-
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-     home: LoginPage(),
-     routes: <String, WidgetBuilder> {
-       '/landingpage': (BuildContext context) => new MyApp(),
-       '/signup': (BuildContext context) => new SignupPage(),
-       '/homepage': (BuildContext context) => new HomePage(),
-     },
+      title: 'EntreCor',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new LoginPage()
     );
   }
 }
+
