2d0e96df59b4e6ef399e70cad22b145983bd1289
Aylin <aylingrgn2@gmail.com>
2018-12-06 02:15:58 +0300

    News Section updates as horizontal cards
    Issues:  Card heights arent same need no decide which height to set ! LAME DUDE
    -Now I can see the article but how to continue ? Should be a drawer expands from the top or directly send to the news ? or even better we can redirect to one of our pages and fetch the API's 'content' parameter !

--- entrecore/lib/NewsPage.dart
+++ entrecore/lib/NewsPage.dart
@@ -3,26 +3,29 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class NewsPage extends StatefulWidget{
+class NewsPage extends StatefulWidget {
   @override
-  NewsPageState createState()=> new NewsPageState();
+  NewsPageState createState() => new NewsPageState();
 }
-class NewsPageState extends State<NewsPage>{
-  final String url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e6dba1fdf08b43a2b2ddc4562d38d0b1';
+
+class NewsPageState extends State<NewsPage> {
+  final String url =
+      'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e6dba1fdf08b43a2b2ddc4562d38d0b1';
   List data;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     this.getJsonData();
   }
 
-  Future<String> getJsonData() async{
-    var res = await http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
-    
+  Future<String> getJsonData() async {
+    var res = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
     setState(() {
-          var resBody = json.decode(res.body);
-          data = resBody["articles"];
+      var resBody = json.decode(res.body);
+      data = resBody["articles"];
     });
 
     return "Success !";
@@ -30,27 +33,48 @@ class NewsPageState extends State<NewsPage>{
 
   @override
   Widget build(BuildContext context) {
-      return new Scaffold(
-        body: new ListView.builder(
-          itemCount: data == null ? 0 : data.length,
-          itemBuilder: (BuildContext context, int index){
-            return new Container(
-              child: new Center(
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
+    return new Scaffold(
+      body: new ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          return SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                new Container(
+                  child: new Text(
+                    data[index]['title'],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  padding: const EdgeInsets.all(20.0),
+                ),
+                new Container(
+                  child: new Text(
+                    data[index]['description'],
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                ),
+                new Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    new Card(
-                      child: new Container(
-                        child: new Text(data[index]['author']),
-                        padding: const EdgeInsets.all(20.0),
-                      ),
-                    )
+                    new Text(
+                      data[index]['author'],
+                      textAlign: TextAlign.left,
+                    ),
                   ],
                 ),
-              ),
-            );
-          },
-        ),
-      );
-    }
+                 new Card(
+                  child: new Text(''), // This is because card looks better Flutter doesn't have vertical dividers yet.
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
